2d643456bbc398c4086361b39817dc50c6a3e583
leo.liu <cucmeliu@gmail.com>
2018-05-30 15:54:11 +0800

    sms

---
+++ toolkit/smsdealer/params.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# author: leo.liu
+# date: 2018.5.22
+# Filename: params
+''' params
+'''
+
+
+class URL(object):
+	def __init__(self):
+		self.name = ''
+		self.url  = ''
+		self.header = ''
+		self.param = ''
+
+
+class URLs(object):
+	def __init__(self):
+		self.urls = []
